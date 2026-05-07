a07444580cbce43b61690e1ef45f2dfbb67d29fd
Renato <renatozmusante@gmail.com>
2026-05-07 18:44:15 -0300

    Mensaje de bienvenida vercel para que no de 404

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,16 @@ const tablesRouter = require("./tables.router");
 const itemsRouter = require("./items.router");
 const paymentsRouter = require("./payments.router");
 const usersRouter = require("./users.router");
+const { sendSuccess } = require("../utils/response.util");
 
 function mountRoutes(app) {
+  app.get("/", (_req, res) => {
+    return sendSuccess(res, 200, {
+      name: "MesaPay API",
+      message:
+        "Si estás leyendo esto, MesaPay API está funcionando :).",
+    });
+  });
   app.use("/auth", authRouter);
   app.use("/tables", tablesRouter);
   app.use("/items", itemsRouter);
